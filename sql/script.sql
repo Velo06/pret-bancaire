17f83049c934508973b9ca9bf68537739e5bd42f
steve-daniel0 <stevens.rakotomalala@gmail.com>
2025-07-07 13:58:29 +0300

    creation fond

--- sql/script.sql
+++ sql/script.sql
@@ -1,11 +1,59 @@
-CREATE DATABASE tp_flight CHARACTER SET utf8mb4;
+CREATE TABLE role_clients (
+    id INT PRIMARY KEY,
+    nom_role VARCHAR(255)
+);
 
-USE tp_flight;
+CREATE TABLE status_clients (
+    id INT PRIMARY KEY,
+    status_role VARCHAR(255)
+);
 
-CREATE TABLE etudiant (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(100),
-    prenom VARCHAR(100),
-    email VARCHAR(100),
-    age INT
+CREATE TABLE clients (
+    id INT PRIMARY KEY,
+    nom VARCHAR(255),
+    username VARCHAR(255),
+    email VARCHAR(255),
+    telephone VARCHAR(255),
+    date_inscription TIMESTAMP,
+    role INT,
+    statut INT,
+    FOREIGN KEY (role) REFERENCES role_clients(id),
+    FOREIGN KEY (statut) REFERENCES status_clients(id)
+);
+
+CREATE TABLE etablissement_financier (
+    id INT PRIMARY KEY,
+    solde_actuelle DECIMAL(15,2)
+);
+
+CREATE TABLE type_pret (
+    id INT PRIMARY KEY,
+    nom VARCHAR(255),
+    taux_interet_annuel INT,
+    duree_max_mois INT
+);
+
+CREATE TABLE etat_validation (
+    id INT PRIMARY KEY,
+    nom_etat_validation VARCHAR(255)
+);
+
+CREATE TABLE historique_emprunt (
+    montant DECIMAL(15,2)
+);
+
+CREATE TABLE pret (
+    id INT PRIMARY KEY,
+    client INT,
+    type_pret_id INT,
+    montant_emprunt INT,
+    date_debut TIMESTAMP,
+    date_fin TIMESTAMP NULL,
+    taux_interet DECIMAL(5,2),
+    id_etat_validation INT,
+    montant_remboursé DECIMAL(15,2),
+    date_creation TIMESTAMP NULL,
+    FOREIGN KEY (client) REFERENCES clients(id),
+    FOREIGN KEY (type_pret_id) REFERENCES type_pret(id),
+    FOREIGN KEY (id_etat_validation) REFERENCES etat_validation(id)
 );
--- ws/index.php
+++ ws/index.php
@@ -12,20 +12,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 require 'vendor/autoload.php';
 require 'db.php';
 
-Flight::route('GET /etudiants', function() {
+Flight::route('GET /etudiants', function () {
     $db = getDB();
     $stmt = $db->query("SELECT * FROM etudiant");
     Flight::json($stmt->fetchAll(PDO::FETCH_ASSOC));
 });
 
-Flight::route('GET /etudiants/@id', function($id) {
+Flight::route('GET /etudiants/@id', function ($id) {
     $db = getDB();
     $stmt = $db->prepare("SELECT * FROM etudiant WHERE id = ?");
     $stmt->execute([$id]);
     Flight::json($stmt->fetch(PDO::FETCH_ASSOC));
 });
 
-Flight::route('POST /etudiants', function() {
+Flight::route('POST /etudiants', function () {
     $data = Flight::request()->data;
     $db = getDB();
     $stmt = $db->prepare("INSERT INTO etudiant (nom, prenom, email, age) VALUES (?, ?, ?, ?)");
@@ -33,7 +33,7 @@ Flight::route('POST /etudiants', function() {
     Flight::json(['message' => 'Étudiant ajouté', 'id' => $db->lastInsertId()]);
 });
 
-Flight::route('PUT /etudiants/@id', function($id) {
+Flight::route('PUT /etudiants/@id', function ($id) {
     $data = Flight::request()->data;
     $db = getDB();
     $stmt = $db->prepare("UPDATE etudiant SET nom = ?, prenom = ?, email = ?, age = ? WHERE id = ?");
@@ -41,11 +41,41 @@ Flight::route('PUT /etudiants/@id', function($id) {
     Flight::json(['message' => 'Étudiant modifié']);
 });
 
-Flight::route('DELETE /etudiants/@id', function($id) {
+Flight::route('DELETE /etudiants/@id', function ($id) {
     $db = getDB();
     $stmt = $db->prepare("DELETE FROM etudiant WHERE id = ?");
     $stmt->execute([$id]);
     Flight::json(['message' => 'Étudiant supprimé']);
+});
+
+// ------------------ Creation fond
+Flight::route('GET /etat_fond', function () {
+    $db = getDB();
+    $stmt = $db->prepare("SELECT * FROM etablissement_financier WHERE id = ?");
+    // 1 par defaut satrie 1 iany ny etablissement financier
+    $stmt->execute([1]);
+
+    Flight::json($stmt->fetch(PDO::FETCH_ASSOC));
+});
+
+Flight::route('POST /creation_fond', function () {
+    $data = Flight::request()->data;
+    $montant = isset($data->montant) ? floatval($data->montant) : 0;
+    $db = getDB();
+
+    if ($montant > 0) {
+        // Historique
+        $stmt = $db->prepare("INSERT INTO historique_emprunt (montant) VALUES (?)");
+        $stmt->execute([$montant]);
+
+        // Mise à jour solde
+        $stmtUpdate = $db->prepare("UPDATE etablissement_financier SET solde_actuelle = solde_actuelle + ? WHERE id = 1");
+        $stmtUpdate->execute([$montant]);
+
+        Flight::json(['message' => 'Montant bien enregistré']);
+    } else {
+        Flight::json(['message' => 'Montant invalide']);
+    }
 });
 
 Flight::start();
