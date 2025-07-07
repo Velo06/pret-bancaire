800094e2696d362c13959f0f271d32b989b6725b
sarobidy178 <antsa.rasm@gmail.com>
2025-07-07 12:08:50 +0300

    changement de db.php

--- ws/db.php
+++ ws/db.php
@@ -2,6 +2,8 @@
 function getDB() {
     $host = '127.0.0.1';
     $dbname = 'tp_flight';
+    $username = 'root'
+    $password = ''
 
     try {
         return new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, [
