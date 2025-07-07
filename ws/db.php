c23c6ca8932d40ecd786878388a7202c2e862ac4
sarobidy178 <antsa.rasm@gmail.com>
2025-07-07 12:09:18 +0300

    correctiond e db.php

--- ws/db.php
+++ ws/db.php
@@ -2,8 +2,8 @@
 function getDB() {
     $host = '127.0.0.1';
     $dbname = 'tp_flight';
-    $username = 'root'
-    $password = ''
+    $username = 'root';
+    $password = '';
 
     try {
         return new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, [
